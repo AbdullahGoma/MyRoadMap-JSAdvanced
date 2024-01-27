77c1f8e83fc1e2189c0c0fcde32693d5a46184d5
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-01-27 03:16:58 +0200

    javaScriptAdavanced

---
+++ First/First.js
@@ -0,0 +1,11 @@
+
+
+var fun = new Function("x", "y", "alert(x+y)");
+
+document.getElementById("#id").click = function(){
+  alert("test");
+};
+
+setInterval(function () {
+  // fun body
+}, 1000)
